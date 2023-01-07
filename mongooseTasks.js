6834a9763f701e79e85c951dcefcca6c1c946f94
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-01-07 23:04:08 +0300

    Создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 mongoose.set('strictQuery', true);
 
-var Orb = mongoose.model('Orb', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var sphere = new Orb({ name: 'Wex' })
+schema.methods.attribute= function(){
+    console.log(this.get("name") + " прибавил атрибуты")
+}
+
+var Orb = mongoose.model('Orb', schema);
+
+var sphere = new Orb({ name: 'Exort' })
 sphere.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('E.M.P.')
-    }
+   sphere.attribute()
 })
-
-
