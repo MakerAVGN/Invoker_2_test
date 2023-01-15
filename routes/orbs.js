88edfaf923adb95dbac5d680b5db3716ddab16e4
MakerAVGN <ivan.shapkin.2015@mail.ru>
2023-01-15 21:31:56 +0300

    Вывод данных на экран

--- routes/orbs.js
+++ routes/orbs.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var Orb = require("../models/orb").Orb
-var async = require("async")
-var checkAuth = require("../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+//var Orb = require("../models/orb").Orb
+// var async = require("async")
+//var checkAuth = require("../middleware/checkAuth.js")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с orbs');
 });
+  
 
 /* Страница сфер */
 
-router.get('/:nick',checkAuth,function(req, res, next) {
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM orbs WHERE orbs.nick = '${req.params.nick}'`, (err, orbs) => {
+  if(err) {
+  console.log(err);
+  if(err) return next(err)
+  } else {
+  if(orbs.length == 0) return next(new Error("Нет такой сферы!"))
+  var orb = orbs[0];
+  res.render('orb', {
+  title: orb.title,
+  picture: orb.avatar,
+  desc: orb.about
+  })
+  // result(null, results);
+  }
+  })
+});
+
+
+/*router.get('/:nick',checkAuth,function(req, res, next) {
     Orb.findOne({nick:req.params.nick}, function(err,orb){
         if(err) return next(err)
         if(!orb) return next(new Error("Нет такой сферы"))
@@ -21,7 +42,7 @@ router.get('/:nick',checkAuth,function(req, res, next) {
             desc: orb.desc
         }) 
     })
-})
+}) */
 
   
   module.exports = router
